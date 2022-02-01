d4606b547d4bcb10ea79c5b5763e58d66a076f98
egoCheers <ego.cheers@gmail.com>
2022-02-01 15:22:38 +0300

    Redirect to login page component test

---
+++ Proinug.Tests/RedirectToLoginPageTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Proinug.WebUI.Components;
+
+namespace Proinug.Tests;
+
+public class RedirectToLoginPageTest
+{
+    [Fact]
+    public void Redirect_NavReturnLoginRelativeUri()
+    {
+        using var ctx = new TestContext();
+        var nav = ctx.Services.GetRequiredService<NavigationManager>();
+
+        var component = ctx.RenderComponent<RedirectToLoginPage>();
+        Assert.Equal("login", nav.ToBaseRelativePath(nav.Uri));
+    }
+}
