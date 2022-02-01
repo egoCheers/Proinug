f2f09bc9e7e17c0bb217d87568de9b404e6124ce
egoCheers <ego.cheers@gmail.com>
2022-02-01 15:16:46 +0300

    Credentials model

---
+++ Proinug.WebUI/ViewModels/CredentialsVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proinug.WebUI.ViewModels;
+
+/// <summary>
+/// Credentials view model (for login form)
+/// </summary>
+public class CredentialsVM
+{
+    [Required]
+    public string Username { get; set; } = "";
+
+    [Required]
+    public string Password { get; set; } = "";
+}
