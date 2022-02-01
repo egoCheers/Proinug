89df9a366d0d0498093578e60efef5ca251508c0
egoCheers <ego.cheers@gmail.com>
2022-02-01 23:53:12 +0300

    RealTimeClock (for tests need to do it this way)

---
+++ Proinug.WebUI/Services/RealSystemClock.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Proinug.WebUI.Services;
+
+public class RealSystemClock: ISystemClock
+{
+    public DateTimeOffset UtcNow => DateTime.UtcNow;
+}
