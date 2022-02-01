356f9def26356bade285c32ee70ce37e77a1a581
egoCheers <ego.cheers@gmail.com>
2022-02-01 23:53:33 +0300

    ServiceCollectionExtensions AddAuthService

---
+++ Proinug.WebUI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Proinug.WebUI.Interfaces;
+using Proinug.WebUI.Services;
+
+namespace Proinug.WebUI.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddAuthService(this IServiceCollection services, IConfiguration configuration)
+    {
+        var authApiBaseAddress = configuration["AuthApiBaseAddress"];
+        services.AddHttpClient<IAuthService, AuthService>(client =>
+        {
+            client.BaseAddress = new Uri(authApiBaseAddress);
+        });
+        return services;
+    }
+
+    // public static IServiceCollection AddKktCloudService(this IServiceCollection services, IConfiguration configuration)
+    // {
+    //     var kktApiBaseAddress = configuration["WebApiBaseAddress"];
+    //     services.AddHttpClient<IKktCloudService, KktCloudService>(client =>
+    //     {
+    //         client.BaseAddress = new Uri(kktApiBaseAddress);
+    //     });
+    //     return services;
+    // }
+    //
+    // public static IServiceCollection AddUsersService(this IServiceCollection services, IConfiguration configuration)
+    // {
+    //     var usersApiBaseAddress = configuration["AuthApiBaseAddress"];
+    //     services.AddHttpClient<IUsersService, UsersService>(client =>
+    //     {
+    //         client.BaseAddress = new Uri((usersApiBaseAddress));
+    //     });
+    //     return services;
+    // }
+}
