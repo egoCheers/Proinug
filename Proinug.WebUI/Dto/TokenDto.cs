19169bdddb141af780c4bdbfe40c58d055b39139
egoCheers <ego.cheers@gmail.com>
2022-02-01 22:15:33 +0300

    JWT data transfer object (which is received from web api)

---
+++ Proinug.WebUI/Dto/TokenDto.cs
@@ -0,0 +1,32 @@
+﻿namespace Proinug.WebUI.Dto;
+
+/// <summary>
+/// Token DTO
+/// </summary>
+public class TokenDto
+{
+    /// <summary>
+    /// Account
+    /// </summary>
+    public AccountDto Account { get; set; } = new();
+
+    /// <summary>
+    /// JSON Web Token
+    /// </summary>
+    public string Jwt { get; set; } = "";
+
+    /// <summary>
+    /// Expiring time
+    /// </summary>
+    public DateTime Expires { get; set; }
+
+    /// <summary>
+    /// Refresh token id
+    /// </summary>
+    public Guid RefreshTokenId { get; set; }
+
+    /// <summary>
+    /// Expiring time in ticks
+    /// </summary>
+    public long ExpiresJsTicks { get; set; }
+}
