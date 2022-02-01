73ff143783b29db41cb3f0bc12b0cf54c360933d
egoCheers <ego.cheers@gmail.com>
2022-02-01 22:13:54 +0300

    Account data transfer object (data from web api)

---
+++ Proinug.WebUI/Dto/AccountDto.cs
@@ -0,0 +1,29 @@
+﻿using Proinug.WebUI.Models;
+
+namespace Proinug.WebUI.Dto;
+
+/// <summary>
+/// Account DTO
+/// </summary>
+public class AccountDto
+{
+    /// <summary>
+    /// Id
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// User name
+    /// </summary>
+    public string Name { get; set; } = "";
+
+    /// <summary>
+    /// User role
+    /// </summary>
+    public Roles Role { get; set; }
+
+    /// <summary>
+    /// Description
+    /// </summary>
+    public string Description { get; set; } = "";
+}
