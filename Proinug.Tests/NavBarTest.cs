e41dd4b28b0dfb59a2e52c8bbd2319ed99997820
egoCheers <ego.cheers@gmail.com>
2022-02-01 10:36:58 +0300

    Tests for Navbar component

---
+++ Proinug.Tests/NavBarTest.cs
@@ -0,0 +1,20 @@
+﻿using Bunit;
+using Proinug.WebUI.Components;
+using Proinug.WebUI.Shared;
+using Xunit;
+
+namespace Proinug.Tests;
+
+public class NavBarTest
+{
+    [Fact]
+    public void Navbar_HaveTopRowClass()
+    {
+        using var ctx = new TestContext();
+        
+        var component = ctx.RenderComponent<Navbar>();
+        var navbar = component.Find("#Navbar");
+        
+        if (navbar.ClassName != null) Assert.Contains("top-row", navbar.ClassName);
+    }
+}
