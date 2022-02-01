2d3f13c73c6482e8a4b14761077506a4c83c4a25
egoCheers <ego.cheers@gmail.com>
2022-02-01 22:14:55 +0300

    User roles

---
+++ Proinug.WebUI/Models/Roles.cs
@@ -0,0 +1,37 @@
+﻿namespace Proinug.WebUI.Models;
+
+/// <summary>
+/// User roles
+/// </summary>
+public enum Roles
+{
+    /// <summary>
+    /// Not authorized
+    /// </summary>
+    NoAuthorized,
+
+    /// <summary>
+    /// Base role
+    /// </summary>
+    Base = 100,
+
+    /// <summary>
+    /// Base application role
+    /// </summary>
+    Application = 110,
+
+    /// <summary>
+    /// Support
+    /// </summary>
+    Support = 200,
+
+    /// <summary>
+    /// Administrator
+    /// </summary>
+    Administrator = 300,
+
+    /// <summary>
+    /// Super administrator
+    /// </summary>
+    SuperAdministrator = 400
+}
