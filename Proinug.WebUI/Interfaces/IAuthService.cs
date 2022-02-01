1d6f16934da952cfb85ae899faf6dc4ba2c69dff
egoCheers <ego.cheers@gmail.com>
2022-02-01 22:14:45 +0300

    User service interface

---
+++ Proinug.WebUI/Interfaces/IAuthService.cs
@@ -0,0 +1,24 @@
+﻿using Proinug.WebUI.Dto;
+using Proinug.WebUI.Models;
+
+namespace Proinug.WebUI.Interfaces;
+
+/// <summary>
+/// Users authentication service
+/// </summary>
+public interface IAuthService
+{
+    /// <summary>
+    /// User Login
+    /// </summary>
+    /// <param name="credentials"></param>
+    /// <returns></returns>
+    Task<(int Error, TokenDto? Token)> LoginAsync(Credentials credentials);
+    
+    /// <summary>
+    /// Refresh token
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    Task<(int Error, TokenDto? Token)> RefreshToken(TokenDto token);
+}
--- Proinug.WebUI/Services/AuthService.cs
+++ Proinug.WebUI/Services/AuthService.cs
@@ -1,6 +1,39 @@
-﻿namespace Proinug.WebUI.Services;
+﻿using Proinug.WebUI.Dto;
+using Proinug.WebUI.Interfaces;
+using Proinug.WebUI.Models;
 
-public class AuthService
+namespace Proinug.WebUI.Services;
+
+/// <summary>
+/// Users authentication service
+/// </summary>
+public class AuthService: IAuthService
 {
-    
+    private readonly HttpClient _client;
+    private readonly ILogger<AuthService> _logger;
+
+    public AuthService(HttpClient client, ILogger<AuthService> logger)
+    {
+        _client = client;
+        _logger = logger;
+    }
+    /// <summary>
+    /// User Login
+    /// </summary>
+    /// <param name="credentials"></param>
+    /// <returns></returns>
+    public Task<(int Error, TokenDto? Token)> LoginAsync(Credentials credentials)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Refresh token
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public Task<(int Error, TokenDto? Token)> RefreshToken(TokenDto token)
+    {
+        throw new NotImplementedException();
+    }
 }
