20bbdf2d9b300a6bd78abb16f446600774b66b36
egoCheers <ego.cheers@gmail.com>
2022-02-01 15:19:10 +0300

    Custom authentication state provider

---
+++ Proinug.WebUI/Services/CwAuthenticationStateProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Proinug.WebUI.Services;
+
+/// <summary>
+/// Custom authentication state provider
+/// </summary>
+public class CwAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private const string USER_SESSION_OBJECT_KEY = "user_session_obj";
+    
+    /// <summary>
+    /// Returns 
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
