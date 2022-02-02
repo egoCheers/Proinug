cf65cd0a1500fc1eaed2ce3d47d98fa25f17b958
egoCheers <ego.cheers@gmail.com>
2022-02-02 16:49:31 +0300

    Interface for CwAuthenticationStateProvider

---
+++ Proinug.WebUI/Interfaces/ICwAuthenticationStateProvider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Proinug.WebUI.Dto;
+using Proinug.WebUI.Models;
+
+namespace Proinug.WebUI.Interfaces;
+
+public interface ICwAuthenticationStateProvider
+{
+    TokenDto? TokenDto { get; }
+
+    /// <summary>
+    /// Returns authentication state
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    Task<AuthenticationState> GetAuthenticationStateAsync();
+
+    /// <summary>
+    /// Login async
+    /// </summary>
+    /// <param name="credentials"></param>
+    /// <returns></returns>
+    Task<int> LoginAsync(Credentials credentials);
+
+    /// <summary>
+    /// Logout async
+    /// </summary>
+    Task LogoutAsync();
+
+    event AuthenticationStateChangedHandler? AuthenticationStateChanged;
+}
